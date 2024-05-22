3a49d60657934cc93c29a27b4302a509a4bb25ad
Sofiiar <sofiiaradul@gmail.com>
2024-05-22 22:43:31 +0200

    PW-3: Added comment with alternative way to locate element

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ def run(playwright: Playwright) -> None:
     page.wait_for_load_state("networkidle")
     page.locator("[data-test=\"username\"]").click()
     page.locator("[data-test=\"username\"]").fill("standard_user")
+    # alternative -> page.fill("input:below(:text(\"Swag Labs\"))", "standard_user")
     page.locator("[data-test=\"password\"]").click()
     page.locator("[data-test=\"password\"]").fill("secret_sauce")
     page.locator("[data-test=\"login-button\"]").click()
